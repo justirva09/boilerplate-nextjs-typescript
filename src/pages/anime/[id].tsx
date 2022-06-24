930fecf627d52b422caa6d9aca7b159a33c96066
Muhammad Irva <muhammad.irva@bibit.id>
2022-06-24 09:01:21 +0700

    update example for react-query

--- src/pages/anime/[id].tsx
+++ src/pages/anime/[id].tsx
@@ -1,23 +1,69 @@
-import { useFetchAnimeInfo } from "@services/example.services";
-import { NextPage } from "next";
+import { AnimeIfo } from "@services/example.interface";
+import { getAnimeDetails, useFetchAnimeInfo } from "@services/example.services";
+import { ApiResponseType } from "@type-general";
+import { GetServerSideProps, NextPage } from "next";
+import { useRouter } from "next/router";
 import React from "react";
+import { dehydrate, QueryClient, useQuery } from "react-query";
+const getPost = () => fetch("https://jsonplaceholder.typicode.com/posts");
 
 const Anime: NextPage = () => {
-  const { data } = useFetchAnimeInfo("20");
+  const router = useRouter();
+  const { isLoading, isError, data } = useFetchAnimeInfo(router.query.id);
+
+  if (isLoading) {
+    return <div className="">Loading...</div>;
+  }
+
+  if (isError) {
+    return <div className="">Error</div>;
+  }
+
+  if (data === undefined) {
+    return <div className="">No data</div>;
+  }
+
   return (
     <>
+      <h1>Anime</h1>
+      <div className="">
+        <img src={data?.images.jpg.image_url} alt={data?.title} />
+      </div>
       <div className="">
         <h4>Title</h4>
         <p>
-          {data?.data.title} / {data?.data.title_japanese}
+          {data?.title} / {data?.title_japanese}
         </p>
       </div>
       <div className="">
         <h4>Synopsis</h4>
-        <p>{data?.data.synopsis}</p>
+        <p>{data?.synopsis}</p>
       </div>
     </>
   );
 };
+
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery("anime", async () => getAnimeDetails(params?.id as string));
+
+//   console.info(queryClient);
+
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
+
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+//   const response: ApiResponseType<AnimeIfo> = await getAnimeDetails(params?.id as string);
+//   const {isLoading, data} = useQuery('anime-details', () => getAnimeDetails(params?.id as string));
+//   return {
+//     props: {
+//       data: response.data.data,
+//     },
+//   };
+// };
 
 export default Anime;
--- src/services/example.services.ts
+++ src/services/example.services.ts
@@ -4,17 +4,26 @@ import { useQuery } from "react-query";
 import { AnimeIfo } from "./example.interface";
 
 // request example
+export const getAnimeDetails = async (id: string | string[] | undefined) =>
+  apiRequest({
+    path: `/anime/${id}/full`,
+    method: "GET",
+  });
 
 // useQuery example
-export const useFetchAnimeInfo = (id: string) =>
-  useQuery(
-    ["anime"],
+export const useFetchAnimeInfo = (id: string | string[] | undefined) => {
+  const { isLoading, isError, data } = useQuery(
+    ["anime", id],
     async () => {
-      const response: ApiResponseType<AnimeIfo> = await apiRequest({
-        path: `/anime/${id}/full`,
-        method: "GET",
-      });
-      return response.data;
+      const response: ApiResponseType<AnimeIfo> = await getAnimeDetails(id);
+      return response.data.data;
     },
-    { refetchOnWindowFocus: true }
+    {
+      refetchOnWindowFocus: true,
+      enabled: !!id, // enable query only if id is provided
+      staleTime: 5 * 1000, // after 5 seconds and the data being stale, it will be refetched
+    }
   );
+
+  return { isLoading, isError, data };
+};
